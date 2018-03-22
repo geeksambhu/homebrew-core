7c7c84736d5fec9cc5a96ad9371b16a0fbd8bd45
Shiva Gaire <geeksambhu@users.noreply.github.com>
2018-03-22 15:18:29 +0545

    For libevent 2.0.21

--- Formula/libevent.rb
+++ Formula/libevent.rb
@@ -1,7 +1,7 @@
 class Libevent < Formula
   desc "Asynchronous event library"
   homepage "http://libevent.org"
-  url "https://github.com/libevent/libevent/archive/release-2.1.8-stable.tar.gz"
+  url "https://github.com/libevent/libevent/archive/release-2.0.21-stable.tar.gz"
   sha256 "316ddb401745ac5d222d7c529ef1eada12f58f6376a66c1118eee803cb70f83d"
 
   bottle do
